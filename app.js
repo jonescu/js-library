f768914f01bb415bc9a6214db50932981cc2366d
Nick Jonescu <nicholasjonescu@gmail.com>
2022-06-21 19:22:40 -0400

    added local storage and redid UI

--- app.js
+++ app.js
@@ -1,88 +1,196 @@
+// Variables
 
-  
-const bookBtn = document.getElementById('book-btn')
-const container = document.querySelector('.container')
-const modal = document.querySelector('.modal')
-const submitBtn = document.querySelector('#submit')
-const author = document.getElementById('author')
-const title = document.getElementById('title')
-const pages = document.getElementById('pages')
-const read = document.getElementById('read')
+const libraryContainer = document.querySelector('.library-container')
+const addBookBtn = document.getElementById('add-book')
+const bookForm = document.querySelector('.book-form')
+const bookTitle = document.getElementById('book-title')
+const bookAuthor = document.getElementById('book-author')
+const bookPages = document.getElementById('book-pages')
+const readSelector = document.getElementById('read-selector')
+const confirmAddBtn = document.getElementById('confirm-add-button')
 
-let myLibrary = [
-  
-];
-//book constructor
-function Book(author, title, pages) {
-    this.author = author
+class Book {
+  constructor(title, author, pages, status){
     this.title = title
+    this.author = author
     this.pages = pages
+    this.status = status
+  }
 }
 
-function displayBooks(array) {
-  //make new object on click and add to library array
-  const myBook = new Book(author.value, title.value, pages.value);
-  myLibrary.push(myBook)
+const library = {
+  books: getBooksFromStorage()
+}
 
-  //create card
-  const newCard = document.createElement('div');
-  newCard.classList.add('book-card')
+function populateUI(){
+  let html = ''
+  library.books.forEach(book => {
+      html += `<div class="book-card">
+      <p>Title: ${book.title}</p>
+      <p>Author: ${book.author}</p>
+      <p>Pages: ${book.pages}</p>
+      <p>Status: ${book.status}</p>
+      <p>Id: ${book.id}</p>
+      <a class="delete-btn">
+        <i class="las la-trash"></i>
+      </a>
+      <button class="toggle-read-button">${book.status}</button>
+      </div>`
+  })
+  libraryContainer.innerHTML = html
+}
 
-  //remove book from library when clicked
-  const removeBook = document.createElement('button')
-  removeBook.classList.add('remove-btn')
-  removeBook.textContent = "Remove Book"
-  removeBook.addEventListener('click', function(e) {
-    e.target.parentElement.style.display = 'none'
+window.addEventListener('DOMContentLoaded', populateUI)
+
+function showForm(){
+  bookForm.style.visibility = 'visible'
+}
+addBookBtn.addEventListener('click', showForm)
+
+function createBook(){
+  if(bookTitle.value === '' || bookAuthor.value === '' || bookPages.value === '' || readSelector.value === '') {
+    alert('please fill in all fields')
+    return false
+  } 
+
+  // Create book
+  const newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, readSelector.value)
+
+  // Assign ID
+  if(library.books.length !== 0) {
+    newBook.id = library.books.length
+  } else {
+    newBook.id = 0
+  }
+
+  // Persist to LS
+  storeBook(newBook)
+
+  // Push to Library
+  library.books.push(newBook)
+
+  // Display in UI
+  displayBook(newBook)
+
+  // Hide form
+  bookForm.style.visibility = 'hidden'
+}
+
+confirmAddBtn.addEventListener('click', createBook)
+
+function displayBook(book){
+
+  // Create book card
+  const bookCard = document.createElement('div')
+
+  let array = []
+
+  // Push object into array and capitalize first letter of each key
+  for(let key in book) {
+   const text = key.charAt(0).toUpperCase() + key.substring(1).toLowerCase() + ": " + `${book[key]}`
+   array.push(text)
+  }
+
+  // Create a P element for each array element
+  array.forEach(function(element) {
+   const newP = document.createElement('p')
+   newP.textContent = element
+   bookCard.appendChild(newP)
   })
 
-  //remove special characters from json except : and , 
-  contents = JSON.stringify(myBook, null, 10).replace(/[`~!@#$%^&*()_|+\-=?;'".<>\{\}\[\]\\\/]/gi, '')
+  const deleteBtn = document.createElement('a')
 
-  //add object contents onto new element, display on screen
-  newCard.textContent = contents;
-  container.appendChild(newCard)
-  newCard.appendChild(removeBook)
-  
-  //assign button class based on whether user has read the book or not.
-  //toggle class when clicked
-  const readBookBtn = document.createElement('button');
-  if(read.checked) {
-    readBookBtn.textContent = "Read"
-    readBookBtn.classList.add('button-read')
-    newCard.appendChild(readBookBtn)
-  } else {
-    readBookBtn.textContent = "Not Read"
-    readBookBtn.classList.add('button-notRead')
-    newCard.appendChild(readBookBtn)
-  }
-  readBookBtn.addEventListener('click', function(){
-    if(readBookBtn.className === "button-read") {
-      readBookBtn.classList.remove('button-read')
-      readBookBtn.classList.add('button-notRead')
-      readBookBtn.textContent = 'Not Read'
-    } else {
-      readBookBtn.classList.remove('button-notRead')
-      readBookBtn.classList.add('button-read')
-      readBookBtn.textContent = 'Read'
-    }
-  }) 
+  const readBtn = document.createElement('button')
+   
+  // Add class to card
+  bookCard.classList.add('book-card')
+
+  // Create delete icon
+  deleteBtn.innerHTML = `<i class="las la-trash"></i>`
+
+  // Add class
+  deleteBtn.classList.add('delete-btn')
+
+  // Add class
+  readBtn.classList.add('toggle-read-button')
+
+  // Add text content
+  readBtn.textContent = readSelector.value
+
+  // Append card, trash icon, and button to library
+  bookCard.appendChild(deleteBtn)
+  bookCard.appendChild(readBtn)
+  libraryContainer.appendChild(bookCard)
+
+  // Clear fields
+  bookTitle.value = ''
+  bookAuthor.value = ''
+  bookPages.value = ''
 }
 
-// Show Modal
-bookBtn.addEventListener('click', function() {
-  modal.style.display = 'block'
-  author.value = ''
-  title.value = ''
-  pages.value = ''
-  read.checked = false;
-})
+libraryContainer.addEventListener('click', deleteBookFromLibrary)
 
-//Hide Modal, check if inputs are empty
-submitBtn.addEventListener('click', function() {
-  if(author.value == '' || title.value == '' || pages.value == '' ) {
+function deleteBookFromLibrary(e) {
+  if(e.target.parentElement.classList.contains('delete-btn')){
+    // Remove from UI
+    e.target.parentElement.parentElement.remove()
+    // Get id
+    const IDString = e.target.parentElement.previousElementSibling.textContent.split(':')
+    const id = IDString[1]
+    // Remove from library
+    library.books.splice(id, 1)
+    // Delete from LS
+    deleteFromStorage(parseInt(id))
+  }
+  e.preventDefault()
+}
+
+libraryContainer.addEventListener('click', toggleRead)
+
+// Toggle read status
+function toggleRead(e){
+  if(e.target.classList.contains('toggle-read-button')) {
+    if(e.target.textContent === "Read") {
+      e.target.textContent = "Not Read"
+      e.target.previousElementSibling.previousElementSibling.previousElementSibling.textContent = "Status: Not Read" 
+    } else {
+      e.target.textContent = "Read"
+      e.target.previousElementSibling.previousElementSibling.previousElementSibling.textContent = "Status: Read" 
+    }
+  }
+e.preventDefault()
+}
+
+// Local Storage
+function storeBook(book){
+  let books;
+  if(localStorage.getItem('books') === null) {
+    books = []
+    books.push(book)
+    localStorage.setItem('books', JSON.stringify(books))
   } else {
-    displayBooks(myLibrary)
-    modal.style.display = 'none'
-};
-})
+    books = JSON.parse(localStorage.getItem('books'))
+    books.push(book)
+    localStorage.setItem('books', JSON.stringify(books))
+  }
+}
+
+function getBooksFromStorage(){
+  let books
+  if(localStorage.getItem('books') === null){
+      books = []
+  } else {
+      books = JSON.parse(localStorage.getItem('books'))
+  }
+  return books
+}
+
+function deleteFromStorage(id){
+    let books = JSON.parse(localStorage.getItem('books'))
+    books.forEach(book => {
+        if(book.id === id){
+            books.splice(book.id, 1)
+        }
+        localStorage.setItem('books', JSON.stringify(books))
+    })
+}
